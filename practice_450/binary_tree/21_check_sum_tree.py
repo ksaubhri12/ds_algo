80a5db55b37cd148272f699bc4c2eae54173cba6
ksaubhrivokal <kalpesh@getvokal.com>
2023-03-30 07:29:45 +0530

    30th March 2023- 2

--- practice_450/binary_tree/21_check_sum_tree.py
+++ practice_450/binary_tree/21_check_sum_tree.py
@@ -1,26 +1,22 @@
 from Node import Node
 
 
-def check_sum_tree_main(root: Node):
-    return check_sum_tree(root)[1]
+def check_sum(root: Node):
+    return check_sum_tree_util(root)[0]
 
 
-def check_sum_tree(root: Node):
+def check_sum_tree_util(root: Node):
     if root is None:
-        return [0, True]
+        return [True, 0]
 
     if root.left is None and root.right is None:
-        return [root.data, True]
+        return [True, root.data]
 
-    left_node_val_check = check_sum_tree(root.left)
-    right_node_val_check = check_sum_tree(root.right)
+    left_tree_check = check_sum_tree_util(root.left)
+    right_tree_check = check_sum_tree_util(root.right)
 
-    sum_check = root.data == left_node_val_check[0] + right_node_val_check[0]
-
-    if sum_check and left_node_val_check[1] and right_node_val_check[1]:
-        return [root.data, True]
-    else:
-        return [root.data, False]
+    value_check = root.data == left_tree_check[1] + right_tree_check[1] and left_tree_check[0] and right_tree_check[0]
+    return [value_check, root.data + left_tree_check[1] + right_tree_check[1]]
 
 
 if __name__ == '__main__':
@@ -29,7 +25,19 @@ if __name__ == '__main__':
     node_3 = Node(2)
 
     node_1.left = node_2
-
     node_1.right = node_3
 
-    print(check_sum_tree_main(node_1))
+    print(check_sum(node_1))
+
+    node_1 = Node(40)
+    node_2 = Node(20)
+    node_3 = Node(30)
+    node_4 = Node(10)
+    node_5 = Node(10)
+
+    node_1.left = node_2
+    node_1.right = node_3
+    node_2.left = node_4
+    node_2.right = node_5
+
+    print(check_sum(node_1))
