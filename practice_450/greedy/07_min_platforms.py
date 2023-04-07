407841ab292095052b6677a64a9606e4bc75abe7
ksaubhrivokal <kalpesh@getvokal.com>
2023-04-07 14:42:04 +0530

    7th April 2023- 2

--- practice_450/greedy/07_min_platforms.py
+++ practice_450/greedy/07_min_platforms.py
@@ -13,8 +13,6 @@ def min_platforms(n, arr, dep):
     return platform_count
 
 
-
-
 if __name__ == '__main__':
     print(min_platforms(6, [900, 940, 950, 1100, 1500, 1800], [910, 1200, 1120, 1130, 1900, 2000]))
     print(min_platforms(3, [900, 1100, 1235], [1000, 1200, 1240]))
